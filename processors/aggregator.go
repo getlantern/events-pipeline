46f5196c24f128bb453a3c755184d9d5ed72c272
Ulysses Aalto <uaalto@getlantern.org>
2016-05-10 14:01:05 -0400

    Simple accumulation

--- processors/aggregator.go
+++ processors/aggregator.go
@@ -4,30 +4,55 @@ import (
 	events "github.com/getlantern/events-pipeline"
 )
 
-type AggregatorOptions struct {
-	FeedbackHandler func(e *events.Event) error
+type AggregationDirective struct {
+	Key            events.Key
+	Val            string
+	AggregatorFunc func(accum, x interface{}) interface{}
+	Identity       interface{}
 }
 
 type Aggregator struct {
 	*events.ProcessorBase
 
-	options *AggregatorOptions
+	directives    []AggregationDirective
+	currentValues []interface{}
 }
 
-func NewAggregator(id string, feedback events.FeedbackFunc, opts *AggregatorOptions) *Aggregator {
+func NewAggregator(id string, feedback events.FeedbackFunc, ds ...AggregationDirective) *Aggregator {
+	initValues := make([]interface{}, len(ds))
+	for i, d := range ds {
+		initValues[i] = d.Identity
+	}
+
 	return &Aggregator{
 		ProcessorBase: events.NewProcessorBase(id, feedback),
-		options:       opts,
+		currentValues: initValues,
+		directives:    ds,
 	}
 }
 
 func (a *Aggregator) Receive(evt *events.Event) error {
 	log.Tracef("AGGREGATOR ID %v PROCESSED event: %v with: %v", a.ID(), evt.Key, evt.Vals)
-	// TODO Processing
+	err := a.ProcessorBase.Receive(evt)
+	if err != nil {
+		return err
+	}
 
-	return a.ProcessorBase.Receive(evt)
+	for i, d := range a.directives {
+		if evt.Key == d.Key {
+			if val, ok := evt.Vals[d.Val]; ok {
+				updated := d.AggregatorFunc(a.currentValues[i], val)
+				a.currentValues[i] = updated
+				evt.Vals[d.Val] = updated
+			}
+		}
+	}
+
+	return a.ProcessorBase.Send(evt)
 }
 
-func (a *Aggregator) Send(evt *events.Event) error {
-	return a.ProcessorBase.Send(evt)
+// Predefined aggregator functions
+
+func AggregatorIntRunningSum(accum, x interface{}) interface{} {
+	return accum.(int) + x.(int)
 }
--- processors/processors_test.go
+++ processors/processors_test.go
@@ -26,10 +26,38 @@ func (s *NullSink) Receive(evt *events.Event) error {
 	return s.SinkBase.Receive(evt)
 }
 
+// Callback Sink
+type CallbackSink struct {
+	*events.SinkBase
+	callback func(e *events.Event)
+}
+
+func NewCallbackSink(id string, cb func(e *events.Event)) *CallbackSink {
+	return &CallbackSink{
+		SinkBase: events.NewSinkBase(id),
+		callback: cb,
+	}
+}
+
+func (c *CallbackSink) Receive(e *events.Event) error {
+	c.callback(e)
+	return c.SinkBase.Receive(e)
+}
+
 func TestAggregator(t *testing.T) {
+	evs := make(chan *events.Event, 3)
+
 	emitter := events.NewEmitterBase("test-emitter", nil)
-	sink := NewNullSink("test-sink")
-	aggregator := NewAggregator("test-aggregator", nil, nil)
+	sink := NewCallbackSink("test-sink", func(e *events.Event) {
+		log.Tracef("Entering callback!")
+		evs <- e
+	})
+
+	aggregator := NewAggregator(
+		"test-aggregator",
+		nil,
+		AggregationDirective{"Karma", "level", AggregatorIntRunningSum, 0},
+	)
 
 	pipeline := events.NewPipeline(emitter)
 
@@ -40,9 +68,19 @@ func TestAggregator(t *testing.T) {
 
 	pipeline.Run()
 
-	emitter.Emit("Key A", &events.Vals{})
-	emitter.Emit("Key B", &events.Vals{})
-	time.Sleep(time.Millisecond * 20)
+	emitter.Emit("Karma", &events.Vals{"level": 20})
+	e := <-evs
+	assert.Equal(t, 20, e.Vals["level"], "Should hold this value")
+
+	emitter.Emit("Karma", &events.Vals{"level": 20})
+	e = <-evs
+	assert.Equal(t, 40, e.Vals["level"], "Should hold this value")
+
+	emitter.Emit("Karma", &events.Vals{"level": 20})
+	e = <-evs
+	assert.Equal(t, 60, e.Vals["level"], "Should hold this value")
+
+	time.Sleep(time.Millisecond * 50)
 
 	pipeline.Stop()
 }
